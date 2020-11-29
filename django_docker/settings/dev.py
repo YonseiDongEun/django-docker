fec773fe2bb26b92db6d8721121a8f8153c5135c
hanqyu <peakjnut37@gmail.com>
2020-11-29 15:10:30 +0900

    fix settings

--- django_docker/settings/dev.py
+++ django_docker/settings/dev.py
@@ -5,10 +5,10 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('POSTGRES_DB'),
-        'USER': env('POSTGRES_USER'),
-        'PASSWORD': env('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': env('POSTGRES_PORT'),
+        'NAME': env('DB'),
+        'USER': env('USER'),
+        'PASSWORD': env('PASSWORD'),
+        'HOST': env('HOST'),
+        'PORT': env('PORT'),
     }
 }
