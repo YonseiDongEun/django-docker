5dd8d3611bd9dc987f3beaa092fbf82baa67873b
hanqyu <peakjnut37@gmail.com>
2020-11-29 15:16:01 +0900

    fix env

--- django_docker/settings/dev.py
+++ django_docker/settings/dev.py
@@ -4,11 +4,11 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB'),
-        'USER': env('USER'),
-        'PASSWORD': env('PASSWORD'),
-        'HOST': env('HOST'),
-        'PORT': env('PORT'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
